5a8182b603c9d10435ec00b12384f1e487f0773b
lailson henrique <lailson.pi@gmail.com>
2019-06-24 18:00:17 -0300

    criando Task

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="tasks">
-        <div v-for="task in tasks" :key="task.name">{{task.name}}</div>
+    <div class="task-grid">
+        <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
     </div>
 </template>
 
 <script>
+import Task from './Task.vue'
 export default {
+    components: {Task},
     props: {
         tasks: {type: Array, required: true}
     }
