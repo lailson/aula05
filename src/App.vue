3595fef8bcb69bb442d2b8b04665a15781c628c1
lailson henrique <lailson.pi@gmail.com>
2019-06-24 17:28:20 -0300

    criando TaskGrid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Tarefas</h1>
+    <TaskGrid :tasks="tasks"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TaskGrid from './components/TaskGrid.vue'
 export default {
+  components: {TaskGrid},
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      tasks: [
+        { name: 'tarefa1', pending: false},
+        { name: 'tarefa2', pending: true},
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+body {
+		font-family: 'Lato', sans-serif;
+		background: #304352;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #d7d2cc, #304352);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #d7d2cc, #304352); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
 </style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tasks">
+        <div v-for="task in tasks" :key="task.name">{{task.name}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: {type: Array, required: true}
+    }
+}
+</script>
+
+<style>
+
+</style>
